20b41d13f761fb89653697c0a91d9003f649bf75
Bourbxn <jakkapat97@gmail.com>
2022-09-19 21:05:04 +0700

    chapter-3 item-3

--- chapter-6/item_3.py
+++ chapter-6/item_3.py
@@ -1,12 +1,17 @@
-def comb_rec(n):
-	
+def comb_bin(n,z):
+    if n>0:
+        comb_bin(n-1,z)
+        print(bin(n)[2:].zfill(z))
+    elif n==0:
+        print(bin(0)[2:].zfill(z))
+    else:
+        print("Only Positive & Zero Number ! ! !")
 
 def main():
-	number = int(input("Enter Number : "))
+    n = int(input("Enter Number : "))
+    z = len(str(bin(2**n-1)[2:])) if n>=0 else 0
+    comb_bin(2**n-1,z)
 
-if __name__ == '__main__'
-	main()
+if __name__ == '__main__':
+    main()
 
-
-Enter Number : -1
-Only Positive & Zero Number ! ! !
